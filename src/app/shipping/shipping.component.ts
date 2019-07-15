2ea519c99931b45c15db4dc7812c6eea94ccb376
Brodie Robertson <brodierobertson54321@gmail.com>
2019-07-15 11:16:27 +0930

    Add some http requests and a shopping cart

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,18 @@
+import { CartService } from './../cart.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent implements OnInit {
+  shippingCosts;
+  constructor(private cartService: CartService) { 
+    this.shippingCosts = cartService.getShippingPrices();
+  }
+
+  ngOnInit() {
+  }
+
+}
